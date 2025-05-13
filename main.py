58e30a609b9bf68d15041da612fc30b65afa2dde
leiyese <ly.leiye@gmail.com>
2025-05-13 13:44:35 +0200

    feat: added main jupyter NB file

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ import seaborn as sns
 # %%
 from load_data import csv_to_df
 
-filename = ""
-df = csv_to_df(filename)
+df_test = csv_to_df("home-data-for-ml-course/test.csv")
+df_train = csv_to_df("home-data-for-ml-course/train.csv")
+
+df_test.head(), df_train.head()
+# %%
